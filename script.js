4ddbfdc08f4cdbddd6db3dd0547102b9549173ef
shahed <shahed.rahman88@gmail.com>
2022-03-21 20:50:39 +0600

    looped input and sotored letters in arr

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 const input = document.getElementById("userInput");
 
+const lettersArr = [];
+
+const val = input.value;
+const reverseVal = "";
 
 const check = () => {
-    const val = input.value;
-    const reverseVal = "";
 
+    // looping through the input value, and insert each letter in to the lettersArr 
+    for(let i =0; i <= val.length; i++ ) {
+        lettersArr.push(val[i]);
+    }
     
 
     input.value=null;
